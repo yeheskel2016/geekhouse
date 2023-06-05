383b73b906d278ebaeca272741da50b81964a198
edo <edoyehezkel@gmail.com>
2023-06-05 16:09:29 +0300

    Merged back model.py into app.py as chatgpt cant solve modular..

--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app, db, login_manager
+from app import app, db, login_manager, User, Station, Reservation
 from forms import LoginForm, RegistrationForm
-from models import User, Station, Reservation
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
